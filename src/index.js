81abdad494c1c29baa4924c1663d1550c6e889fe
cerah <skylark.whaleshark@gmail.com>
2026-09-13 01:29:13 -0500

    webpack update

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import "./main.scss"; // loads all css
+
+let spaceInclude = include('header');
+spaceInclude = include('sidebar1');
+spaceInclude = include('sidebar2');
+spaceInclude = include('footer');
+
+function include(space) {
+  fetch('/src/blocks/' + space + '.html')
+    .then(response => {
+      if (!response.ok) throw new Error('Failed to load ' + space + '.html');
+      return response.text(); // Get content as text
+    })
+    .then(html => {
+      document.getElementById(space + '-container').innerHTML = html; // Insert content
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      document.getElementById(space + '-container').innerHTML = '<p>' + space + ' failed to load.</p>';
+    });
+}
+
+// Code from https://www.lynxbee.com/javascript-display-random-image-refresh/
+const images = [
+    '/src/images/masthead/boat.png',
+    '/src/images/masthead/girl.png',
+    '/src/images/masthead/lillies.png',
+    '/src/images/masthead/river.png',
+    '/src/images/masthead/sphinx.png',
+    '/src/images/masthead/temple.png',
+    '/src/images/masthead/temple2.png',
+    '/src/images/masthead/water.png'
+];
+ 
+window.addEventListener('DOMContentLoaded', () => {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const mastheadImage = document.getElementById('masthead');
+    if (mastheadImage) {
+        mastheadImage.src = images[randomIndex];
+    }
+});
