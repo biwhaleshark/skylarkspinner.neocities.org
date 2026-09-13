18d0b4630a6d9d06584294209e65a3efc43be966
cerah <skylark.whaleshark@gmail.com>
2026-09-13 02:02:15 -0500

    correct file pathing 8

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ spaceInclude = include('sidebar2');
 spaceInclude = include('footer');
 
 function include(space) {
-  fetch('/blocks/' + space + '.html')
+  fetch('./blocks/' + space + '.html')
     .then(response => {
       if (!response.ok) throw new Error('Failed to load ' + space + '.html');
       return response.text(); // Get content as text
@@ -22,14 +22,14 @@ function include(space) {
 
 // Code from https://www.lynxbee.com/javascript-display-random-image-refresh/
 const images = [
-    '/images/masthead/boat.png',
-    '/images/masthead/girl.png',
-    '/images/masthead/lillies.png',
-    '/images/masthead/river.png',
-    '/images/masthead/sphinx.png',
-    '/images/masthead/temple.png',
-    '/images/masthead/temple2.png',
-    '/images/masthead/water.png'
+    './images/masthead/boat.png',
+    './images/masthead/girl.png',
+    './images/masthead/lillies.png',
+    './images/masthead/river.png',
+    './images/masthead/sphinx.png',
+    './images/masthead/temple.png',
+    './images/masthead/temple2.png',
+    './images/masthead/water.png'
 ];
  
 window.addEventListener('DOMContentLoaded', () => {
