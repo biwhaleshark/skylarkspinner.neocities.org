cf7ba395645f30a061ce82ad6fac76898c3293c7
cerah <skylark.whaleshark@gmail.com>
2026-09-13 22:52:48 -0500

    big site update yayyy

--- index.js
+++ index.js
@@ -1,28 +1,8 @@
 import "./main.scss"; // loads all css
 
-let spaceInclude = include('header');
-spaceInclude = include('sidebar1');
-spaceInclude = include('sidebar2');
-spaceInclude = include('footer');
-
-function include(space) {
-  fetch('/blocks/' + space + '.html')
-    .then(response => {
-      if (!response.ok) throw new Error('Failed to load ' + space + '.html');
-      return response.text(); // Get content as text
-    })
-    .then(html => {
-      document.getElementById(space + '-container').innerHTML = html; // Insert content
-    })
-    .catch(error => {
-      console.error('Error:', error);
-      document.getElementById(space + '-container').innerHTML = '<p>' + space + ' failed to load.</p>';
-    });
-}
-
 const tabTitle = document.title; 
 if (tabTitle !== "skylark grotto") { 
-	document.title = tabTitle + " - skylark grotto"; 
+	document.title = tabTitle + " ● skylark grotto"; 
 }
 
 // Code from https://www.lynxbee.com/javascript-display-random-image-refresh/
